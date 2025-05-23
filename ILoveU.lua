21922b5d18fa13b44d35a8e99346b17651145feb
Kyaa <khoaispr01234@gmail.com>
2025-05-23 19:00:36 +0700

    Add files via upload

---
+++ ILoveU.lua
@@ -0,0 +1,9 @@
+function a(v)
+game.StarterGui:SetCore("SendNotification", {
+		Title = "Kyaa Hub",
+		Text = v,
+		Duration = 20
+})
+end
+
+a("The script is currently unavailable. Please try again another time.")
